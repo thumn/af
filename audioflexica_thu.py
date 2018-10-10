ac8f7229d4d232d4b294f195c0d570fd8fff8060
Thu Nguyen <tmnguyen@berkeley.edu>
2018-10-10 15:26:20 -0700

    git outta here

--- audioflexica_thu.py
+++ audioflexica_thu.py
@@ -9,19 +9,23 @@ class Mesh:
         self.app = QtGui.QApplication(sys.argv)
         self.view = gl.GLViewWidget()
         self.view.show()
-        verts = np.array([
-            [0, 0, 0],
-            [2, 0, 0],
-            [1, 2, 0],
-            [1, 1, 1],
+        vertices = []
+        faces = []
+        for row in range(32):
+            for col in range(32):
+                vertices.append([col, row])
+        for i in range(32):
+            for j in range(i+1, 32):
+                for k in range(j+1, 32):
+                    faces.append([i, j, k])
+        vertices = np.array(vertices)
+        faces = np.array(faces)
+        colors = np.array([
+            [1, 0, 0, 0.5],
+            [0, 1, 0, 0.5],
+            [0, 0, 1, 0.5]
         ])
-        faces = np.array([
-            [0, 1, 2],
-            [0, 1, 3],
-            [0, 2, 3],
-            [1, 2, 3]
-        ])
-        self.mesh = gl.GLMeshItem(vertexes=verts, faces=faces)
+        self.mesh = gl.GLMeshItem(vertexes=vertices, faces=faces, faceColors=colors)
         self.mesh.setGLOptions("additive")
         self.view.addItem(self.mesh)
         self.view.setWindowTitle('Meshy')
@@ -29,6 +33,15 @@ class Mesh:
     def run(self):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
 	           QtGui.QApplication.instance().exec_()
+
+def generate_grid():
+        vert = []
+        for j in range(32):
+            for i in range(32):
+                v = [i/32, j/32, 0]
+                vert.append(v)
+        vert = np.array(vert)
+        return vert
 
 if __name__ == '__main__':
     m = Mesh()
